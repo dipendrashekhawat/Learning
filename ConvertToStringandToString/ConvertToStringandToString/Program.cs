6eeeda3f44fadc75ddb7d2283319fe59098c7478
Dipendra.Shekhawat <dipendra.shekhawat@greenorbit.com>
2019-07-30 11:14:17 +0530

    program to demonstarte the difference between Convert.ToString and .ToString methods

---
+++ ConvertToStringandToString/ConvertToStringandToString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ConvertToStringandToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string name;
+                object value = null;
+
+                name = value.ToString(); // Doesn't handle null values throw exception, uncomment this line to check
+                name = Convert.ToString(value); //Handles null values
+
+                Console.WriteLine("Null value is handles and Convert.ToString() doesn't throw NULL exception.");
+            }
+            catch (NullReferenceException nullReferenceException)
+            {
+                Console.WriteLine(".ToString() Method doesn't handle NULL values. Error Message: " + nullReferenceException.Message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Exception occurred: " + exception.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
